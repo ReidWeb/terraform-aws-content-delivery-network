fd67ef5a303277d33d1357db581e2d5abcbd8e3a
Peter Reid <peter@reidweb.com>
2018-09-17 18:28:15 +0100

    refactor(lambdas): Update outputs

--- modules/lambda-iam-role/outputs.tf
+++ modules/lambda-iam-role/outputs.tf
@@ -1,3 +1,4 @@
 output "role_arn" {
+  description = "ARN of role created for use by Lambdas"
   value = "${aws_iam_role.main.arn}"
 }
--- modules/lambdas/outputs.tf
+++ modules/lambdas/outputs.tf
@@ -1,7 +1,14 @@
-output "headers_lambda_name" {
-  value = "${aws_lambda_function.headersLambda.function_name}"
+output "headers_lambda_qualified_arn," {
+  value = "${aws_lambda_alias.headersLambdaAlias.arn}"
+  description = "Qualified ARN of headers Lambda"
 }
 
-output "paths_lambda_name" {
-  value = "${aws_lambda_function.pathsLambda.function_name}"
+output "paths_lambda_qualified_arn," {
+  value = "${aws_lambda_alias.pathsLambdaAlias.arn}"
+  description = "Qualified ARN of paths Lambda"
+}
+
+output "lambda_role_arn" {
+  value = "${module.lambda_iam_role.role_arn}"
+  description = "ARN of role assigned to Lambdas"
 }
