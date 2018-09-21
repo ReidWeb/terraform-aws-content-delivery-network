062480157817856ec94d03bbf4fa00cdc4134513
Peter Reid <peter@reidweb.com>
2018-09-21 11:53:21 +0100

    feat(paths-lambda): Paths lambda no longer relies on hard coded domains

--- modules/lambdas/paths.js
+++ modules/lambdas/paths.js
@@ -1,6 +1,5 @@
 exports.handler = (event, context, callback) => {
   const request = event.Records[0].cf.request;
-  const uri = request.uri;
 
   // Redirect some popular search results to their new pages
   const redirects = [
@@ -10,46 +9,15 @@ exports.handler = (event, context, callback) => {
     { test: /^\/redirection\/?$/g, targetURI: "/contact/" }
   ];
 
-  const buckets = [
-    {
-      bucketName: "gatsby.dev.aws.reidweb.com.s3.amazonaws.com",
-      primaryHost: "gatsby.dev.aws.reidweb.com"
-    },
-    {
-      bucketName: "gatsby.np.aws.reidweb.com.s3.amazonaws.com",
-      primaryHost: "gatsby.np.aws.reidweb.com"
-    },
-    {
-      bucketName: "gatsby.prod.aws.reidweb.com.s3.amazonaws.com",
-      primaryHost: "reidweb.com"
-    }
-  ];
-  const redirect = redirects.find(r => uri.match(r.test));
+  const redirect = redirects.find(r => request.uri.match(r.test));
   if (redirect) {
-    const host = buckets.find(b => b.bucketName === request.headers.host[0].value);
-    const newLocation = `https://${host.primaryHost}${redirect.targetURI}`;
-    const response = {
-      status: "301",
-      statusDescription: "Moved Permanently",
-      headers: {
-        location: [
-          {
-            key: "Location",
-            value: newLocation
-          }
-        ]
-      }
-    };
-
-    console.log(`INFO:Redirecting user to: ${newLocation}`);
-    callback(null, response);
-    return;
+    request.uri = redirect.targetURI;
   }
 
   // Make sure directory requests serve index.html
-  if (uri.endsWith("/")) {
+  if (request.uri.endsWith("/")) {
     request.uri += "index.html";
-  } else if (!uri.includes(".")) {
+  } else if (!request.uri.includes(".")) {
     request.uri += "/index.html";
   }
 
