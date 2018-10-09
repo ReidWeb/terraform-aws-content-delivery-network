0f06495be08d3fbb3010bbfcf76a93e9f9becc49
Peter Reid <peter@reidweb.com>
2018-10-09 15:48:57 +0100

    feat(lambdas): Update security policy headers

--- modules/lambdas/headers.js
+++ modules/lambdas/headers.js
@@ -47,9 +47,9 @@ exports.handler = (event, context, callback) => {
 		{
 			key: "Content-Security-Policy",
 			value:
-				"default-src 'self' ; script-src 'self' 'unsafe-inline'; " +
-				"style-src 'self' 'unsafe-inline' ; img-src 'self' data:; " +
-				"font-src 'self' ; manifest-src 'self' ; " +
+        "default-src 'self' ; script-src 'self' 'unsafe-inline' https://www.google-analytics.com; " +
+        "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://fonts.gstatic.com; img-src 'self' data:; " +
+        "font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com ; manifest-src 'self' ; " +
 				"upgrade-insecure-requests; block-all-mixed-content; "
 		}
 	].forEach(h => (headers[h.key.toLowerCase()] = [h]));
